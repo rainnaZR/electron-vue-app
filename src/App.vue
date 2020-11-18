c4ee8d20356c3af41b51badf1f037670928ed9a2
xiaoteng <tengyongzhi@linctex.com>
2020-11-19 00:45:34 +0800

    feat: 窗口控制功能添加

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <Header />
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="123123Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import Header from './components/Header';
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
+    Header,
     HelloWorld
   }
 }
---
+++ src/components/Header/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <button @click="onWindowMax">最大化窗口</button>
+        <button @click="onWindowMin">最小化窗口</button>
+        <button @click="onWindowRestore">恢复窗口</button>
+        <button @click="onWindowClose">关闭窗口</button>
+    </div>
+</template>
+
+<script>
+const { remote } = window.require('electron');
+
+export default {
+    methods: {
+        onWindowMax(){
+            remote.getCurrentWindow().maximize();
+        },
+        onWindowMin(){
+            remote.getCurrentWindow().minimize();
+        },
+        onWindowRestore(){
+            remote.getCurrentWindow().restore();
+        },
+        onWindowClose(){
+            remote.getCurrentWindow().close();
+        }
+    }
+}
+</script>
